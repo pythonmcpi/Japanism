6ce2f6fbb28086322938d7d7d17a8a9d5801c6e6
Python Minecraft Raspberry Pi <palm.tree6555365@gmail.com>
2020-02-24 16:11:06 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from disutils.core import setup
+
+config = {
+    "description": "A game based on the old social hierarchy of Japan",
+    "author": "Mason Kuan(pythonmcpi)",
+    "url": "https://github.com/pythonmcpi/Japanism",
+    "download_url": "https://github.com/pythonmcpi/Japanism",
+    "version": "0.1.0",
+    "install_requires": [],
+    "packages": ["Japanism"],
+    "scripts": [],
+    "name": "Japanism",
+    "python_requires": ">=3.6"
+}
